1a9102a17bfd3f82665d5c0034d97c0ce899a5db
yuhengen <yuhengen@gmail.com>
2021-02-06 16:03:09 +0000

    Added routes to each component. Styled the site

--- mist/src/App.js
+++ mist/src/App.js
@@ -13,39 +13,69 @@ import {
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
 
 import Home from './components/Home'
 import Profile from './components/Profile'
+import Store from './components/Store'
+import About from './components/About'
+import Library from './components/Library'
+import Login from './components/Login'
 
 function App() {
     return (
-        <Router>
-            <Navbar bg="dark" variant='dark' expand="lg">
-                <Navbar.Brand><Link to='/'><img src={require('./images/mist-logo.png').default} alt='mistlogo' height='70px'/></Link></Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <Switch>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Route exact path='/profile'>
-                    <Profile />
-                </Route>
-            </Switch>
-        </Router>
+
+        <div style={{
+            'backgroundImage': 'url("https://wallpaperaccess.com/full/800244.jpg")',
+            'backgroundSize': 'cover',
+            'backgroundPosition': 'center',
+            'backgroundRepeat': 'no-repeat',
+            'height': '100vh',
+            'width': '100vw',
+            'zIndex': '-1'
+        }}>
+            <Router>
+                <Navbar bg="dark" variant='dark' expand="lg">
+                    <Navbar.Brand as={Link} to="/"><img src={require('./images/mist-logo.png').default} alt='mistlogo' height='70px' /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link as={Link} to="/store">STORE</Nav.Link>
+                            <Nav.Link as={Link} to="/about">ABOUT</Nav.Link>
+                            <NavDropdown title="USERNAME" id="basic-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="/library">LIBRARY</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/profile">PROFILE</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#">LOGOUT</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link as={Link} to="/login">LOGIN</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Container style={{ 'color': 'white' }}>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home />
+                        </Route>
+                        <Route exact path='/store'>
+                            <Store />
+                        </Route>
+                        <Route exact path='/about'>
+                            <About />
+                        </Route>
+                        <Route exact path='/library'>
+                            <Library />
+                        </Route>
+                        <Route exact path='/profile'>
+                            <Profile />
+                        </Route>
+                        <Route exact path='/login'>
+                            <Login />
+                        </Route>
+                    </Switch>
+                </Container>
+            </Router >
+        </div>
     )
 }
 
---
+++ mist/src/components/Library.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Library() {
+    return (
+        <React.Fragment>
+            <h1>Library</h1>
+        </React.Fragment>
+    )
+}
